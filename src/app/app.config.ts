290b80e18a480fc4860d05ce0be2561ec9b904d0
yunus-cookies <yunusokutan@mail.com>
2026-01-04 13:20:22 +0100

    Remove keys

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,13 +8,13 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 const firebaseConfig = {
-  apiKey: 'REMOVED',
-  authDomain: 'majs-ai.firebaseapp.com',
-  databaseURL: 'https://majs-ai-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'majs-ai',
-  storageBucket: 'majs-ai.firebasestorage.app',
-  messagingSenderId: '519964310076',
-  appId: '1:519964310076:web:136ea44addb1ce3d772139',
+  apiKey: process.env['FIREBASE_KEY'],
+  authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+  databaseURL: process.env['FIREBASE_DATABASE_URL'],
+  projectId: process.env['FIREBASE_PROJECT_ID'],
+  storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+  messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
+  appId: process.env['FIREBASE_APP_ID'],
 };
 
 export const appConfig: ApplicationConfig = {
