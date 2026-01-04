97c55cc45daf16fb0c4afb04c3589a7f2ae54fc4
yunus-cookies <yunusokutan@mail.com>
2026-01-04 14:12:47 +0100

    wip

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,12 +9,12 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 const firebaseConfig = {
   apiKey: process.env['FIREBASE_KEY'],
-  authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
-  databaseURL: process.env['FIREBASE_DATABASE_URL'],
-  projectId: process.env['FIREBASE_PROJECT_ID'],
-  storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
-  messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'],
-  appId: process.env['FIREBASE_APP_ID'],
+  authDomain: 'majs-ai.firebaseapp.com',
+  databaseURL: 'https://majs-ai-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'majs-ai',
+  storageBucket: 'majs-ai.firebasestorage.app',
+  messagingSenderId: '519964310076',
+  appId: '1:519964310076:web:136ea44addb1ce3d772139',
 };
 
 export const appConfig: ApplicationConfig = {
